4ac828b430f861754b5b9ee3b2b9a057434b5bea
Dino Work <dino.work@hotmail.com>
2019-12-25 15:44:25 -0300

    Docker com ajuste no migration flyway

--- rest-with-spring-boot-udemy-migrations/src/main/resources/db/migration/V2__Populate_Table_Person.sql
+++ rest-with-spring-boot-udemy-migrations/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -1,4 +1,5 @@
-INSERT INTO person (id, address, first_name, gender, last_name) VALUES(1, 'Uberlândia - Minas Gerais - Brasil', 'Leandro', 'Male', 'Costa');
-INSERT INTO person (id, address, first_name, gender, last_name) VALUES(2, 'Uberlândia - Minas Gerais - Brasil', 'Gabriela', 'Female', 'Silva');
-INSERT INTO person (id, address, first_name, gender, last_name) VALUES(3, 'São Paulo - São Paulo - Brasil', 'João', 'Male', 'Passos');
-INSERT INTO person (id, address, first_name, gender, last_name) VALUES(4, 'São Paulo - São Paulo - Brasil', 'Sandra', 'Female', 'Oliveira');
+INSERT INTO rest_with_spring_boot_udemy.person (id, address, first_name, gender, last_name) VALUES
+(1, 'Uberlândia - Minas Gerais - Brasil', 'Leandro', 'Male', 'Costa'),
+(2, 'Uberlândia - Minas Gerais - Brasil', 'Gabriela', 'Female', 'Silva'),
+(3, 'São Paulo - São Paulo - Brasil', 'João', 'Male', 'Passos'),
+(4, 'São Paulo - São Paulo - Brasil', 'Sandra', 'Female', 'Oliveira');
