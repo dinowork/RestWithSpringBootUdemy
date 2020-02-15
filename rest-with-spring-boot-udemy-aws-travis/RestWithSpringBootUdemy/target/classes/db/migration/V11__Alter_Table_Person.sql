2f32295a837f52a10897841c84a30869e55b4d53
Dino Work <dino.work@hotmail.com>
2020-02-15 18:06:47 -0300

    Integração continua - remoção do target

--- rest-with-spring-boot-udemy-aws-travis/RestWithSpringBootUdemy/target/classes/db/migration/V11__Alter_Table_Person.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE person
-	ADD COLUMN enabled BIT(1) NOT NULL DEFAULT 1 AFTER last_name;
--- rest-with-spring-boot-udemy-aws-travis/RestWithSpringBootUdemy/target/classes/db/migration/V2__Insert_Data_In_Person.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO person (address, first_name, gender, last_name) VALUES('Uberlândia - Minas Gerais - Brasil', 'Leandro', 'Male', 'Costa');
-INSERT INTO person (address, first_name, gender, last_name) VALUES('Uberlândia - Minas Gerais - Brasil', 'Gabriela', 'Female', 'Silva');
-INSERT INTO person (address, first_name, gender, last_name) VALUES('São Paulo - São Paulo - Brasil', 'João', 'Male', 'Passos');
-INSERT INTO person (address, first_name, gender, last_name) VALUES('São Paulo - São Paulo - Brasil', 'Sandra', 'Female', 'Oliveira');
